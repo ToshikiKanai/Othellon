53a5eaff60f8e6d81b6e279532e0b1c6938b9681
tossy <50226151+ToshikiKanai@users.noreply.github.com>
2021-02-01 21:28:14 +0900

    Initial Commit

---
+++ Othellon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Othellon
+//
+//  Created by 金井俊樹 on 2021/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
